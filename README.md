f429bf4e30add2c8e31d84a5db685bfffabb0985
Frans Allen <git.a@upset.dev>
2020-10-02 08:36:59 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <a href="https://upset.dev">
+    <img src="https://avatar.statically.workers.dev/A?rounded=true" alt="Avatar" height="60"/>
+  </a>
+</p>
+
+<h1 align="center">Avatar</h1>
+
+<p align="center">A simple and beautiful avatar service on Cloudflare Workers.</p>
+
+<p align="center">
+  <a href="https://upset.dev">upset.dev</a> |
+  <a href="https://twitter.com/fransallen">Twitter</a> |
+  <a href="https://www.patreon.com/fransallen">Donate</a>
+</p>
+
+## :zap: API
+
+`https://avatar.statically.workers.dev/:text`
+
+- `?s=` change avatar size
+- `?rounded=true` change shape to be round
---
+++ index.js
@@ -0,0 +1,57 @@
+async function handleRequest(request) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    let size = '60';
+    let text = 'A';
+    let radius = '';
+    let color1 = await getRandomColor();
+    let color2 = await getRandomColor();
+
+    if (url.searchParams.has('s')) size = url.searchParams.get('s');
+    if (url.searchParams.get('rounded') == "true") radius = `style="border-radius: 50%;"`;
+    if (path !== '/') text = path.slice(1).substr(0, 2);
+
+    const avatar = `<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg ${radius} width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <g>
+    <defs>
+      <linearGradient id="avatar" x1="0" y1="0" x2="1" y2="1">
+        <stop offset="0%" stop-color="${color1}"/>
+        <stop offset="100%" stop-color="${color2}"/>
+      </linearGradient>
+    </defs>
+    <rect fill="url(#avatar)" x="0" y="0" width="${size}" height="${size}"/>
+    <text x="50%" y="50%" alignment-baseline="central" dominant-baseline="central" text-anchor="middle" fill="#fff" font-family="sans-serif" font-size="${(size * 0.9) / text.length}">${text}</text>
+  </g>
+</svg>
+`;
+
+    const response = new Response(avatar);
+
+    if (path === '/') {
+        // no cache
+        response.headers.set('Cache-Control', 'no-cache');
+    } else {
+        // Set cache for 1 year
+        response.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
+    }
+
+    response.headers.set('Content-Type', 'image/svg+xml; charset=utf8');
+
+    return response;
+}
+
+async function getRandomColor() {
+  var letters = '0123456789abcdef';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request));
+});
