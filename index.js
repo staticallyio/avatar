98ea13561874cff2e1e9fc29cfe57c1f946c5389
Frans Allen <git.a@upset.dev>
2020-10-11 03:50:14 +0700

    Pretty: update tab width to 2

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 async function handleRequest(request) {
-    const url = new URL(request.url);
-    const path = url.pathname;
-    const avatarText = path.replace(BASEPATH, '');
+  const url = new URL(request.url);
+  const path = url.pathname;
+  const avatarText = path.replace(BASEPATH, '');
 
-    let size = '60';
-    let text = 'A';
-    let radius = '';
-    let color1 = await getRandomColor();
-    let color2 = await getRandomColor();
+  let size = '60';
+  let text = 'A';
+  let radius = '';
+  let color1 = await getRandomColor();
+  let color2 = await getRandomColor();
 
-    if (url.searchParams.has('s')) size = url.searchParams.get('s');
-    if (url.searchParams.get('shape') == 'circle')
-        radius = `style="border-radius: 50%;"`;
-    if (url.searchParams.get('shape') == 'rounded')
-        radius = `style="border-radius: 10px;"`;
-    if (path.startsWith(BASEPATH)) text = avatarText.substr(0, 2);
+  if (url.searchParams.has('s')) size = url.searchParams.get('s');
+  if (url.searchParams.get('shape') == 'circle')
+    radius = `style="border-radius: 50%;"`;
+  if (url.searchParams.get('shape') == 'rounded')
+    radius = `style="border-radius: 10px;"`;
+  if (path.startsWith(BASEPATH)) text = avatarText.substr(0, 2);
 
-    let fontsize = (size * 0.9) / text.length;
-    const avatar = `<?xml version="1.0" standalone="no"?>
+  let fontsize = (size * 0.9) / text.length;
+  const avatar = `<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg ${radius} width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" version="1.1" xmlns="http://www.w3.org/2000/svg">
   <g>
@@ -33,33 +33,33 @@ async function handleRequest(request) {
 </svg>
 `;
 
-    const response = new Response(avatar);
+  const response = new Response(avatar);
 
-    if (path === '/') {
-        // no cache
-        response.headers.set('Cache-Control', 'no-cache');
-    } else {
-        // Set cache for 1 year
-        response.headers.set(
-            'Cache-Control',
-            'public, max-age=31536000, immutable',
-        );
-    }
+  if (path === '/') {
+    // no cache
+    response.headers.set('Cache-Control', 'no-cache');
+  } else {
+    // Set cache for 1 year
+    response.headers.set(
+      'Cache-Control',
+      'public, max-age=31536000, immutable',
+    );
+  }
 
-    response.headers.set('Content-Type', 'image/svg+xml; charset=utf8');
+  response.headers.set('Content-Type', 'image/svg+xml; charset=utf8');
 
-    return response;
+  return response;
 }
 
 async function getRandomColor() {
-    var letters = '0123456789abcdef';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789abcdef';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
+  event.respondWith(handleRequest(event.request));
 });
